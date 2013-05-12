fffb2438dea31c16c7139a113f853f6edeb3a6cf
Teh Whale <tehwhale@live.com>
2013-05-11 20:20:47 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ElixirThread::Application.config.secret_key_base = '85105806ac324200e1ca6b26c57b6f0bae5060adf2d698acd7650bbf09cc8368623aeb6abdb1563ff4a9da7c1532ed92d3f9214349f8918a13cf70336a9226ec'
