a78df05090dd31085c06bac31945db5fb38d218d
Teh Whale <tehwhale@live.com>
2013-06-03 18:30:38 -0400

    Fixed username having to be the same case as stored in the db

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :login
+
   has_many :posts
 
   has_attached_file :avatar, :styles => { :medium => "100x300>", :thumb => "80x80>" }, :default_url => "/assets/:style/missing.png"
@@ -12,7 +14,12 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :username, :case_sensitive => false
   validates_presence_of :username
 
-  def self.find_for_authentication(tainted_conditions)
-    find_first_by_auth_conditions(tainted_conditions)
+  def self.find_first_by_auth_conditions(warden_conditions)
+  	conditions = warden_conditions.dup
+  	if username = conditions.delete(:username)
+  		where(conditions).where(["lower(username) = :value ", { :value => username.downcase }]).first
+  	else
+  		where(conditions).first
+  	end
   end
 end
